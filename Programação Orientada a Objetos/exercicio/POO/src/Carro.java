710e7631d171cc4ce1dbf0673f2cf4f98d96b939
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2022-06-24 02:49:17 -0300

    P.O.O.

---
+++ Programação Orientada a Objetos/exercicio/POO/src/Carro.java
@@ -0,0 +1,40 @@
+public class Carro {
+    String cor;            // São atributos
+    String modelo;
+    double capacidadeDoTanque;
+
+    carro(String cor, String modelo, double capacidadeDoTanque) {
+        this.cor = cor;    //método especial - Construtor
+        this.modelo = modelo;
+        this.capacidadeDoTanque = capacidadeDoTanque;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;   // método
+    }
+    String getCor() {
+        return cor;
+    }
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    String getModelo() {
+        return modelo;
+    }
+    void setCapacidadeDoTanque(double capacidadeDoTanque) {
+        this.capacidadeDoTanque = capacidadeDoTanque;
+    }
+    double getCapacidadeDoTanque() {
+        return capacidadeDoTanque;
+    }
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeDoTanque = valorCombustivel;
+    }
+
+    /*
+
+    void finalize() {
+                       método especial - Destrutor
+    }
+     */
+}
